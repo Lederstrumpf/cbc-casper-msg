1348b33efe05abbdbbf0ee24609893a3fb8b48fa
Robert Hambrock <roberthambrock@gmail.com>
2018-11-12 14:09:56 +0100

    Regenerify add_message(...)
    Relies on the hacky (_, None) match case in Block::mk_estimate. This is needed
    because the mk_estimate(...) function of the Estimate trait does not yet take
    the message sender as an argument. This will be added in the following commit.

--- src/message.rs
+++ src/message.rs
@@ -401,11 +401,14 @@ mod message {
     use std::{f64};
     use super::*;
 
-    fn add_message<'z>(
-        state: &'z mut BTreeMap<u32, SenderState<BlockMsg>>,
-        sender: u32,
-        recipients: HashSet<u32>,
-    ) -> &'z BTreeMap<u32, SenderState<BlockMsg>> {
+    fn add_message<'z, M>(
+        state: &'z mut BTreeMap<M::Sender, SenderState<M>>,
+        sender: M::Sender,
+        recipients: HashSet<M::Sender>,
+    ) -> &'z BTreeMap<M::Sender, SenderState<M>>
+    where
+        M: CasperMsg
+    {
         // println!("{:?} {:?}", sender, recipients);
         let latest_honest_msgs = LatestMsgsHonest::from_latest_msgs(
             &state[&sender].get_latest_msgs(),
@@ -423,12 +426,12 @@ mod message {
         let estimate = latest_honest_msgs.mk_estimate(
             None,
             state[&sender].get_senders_weights(),
-            Some(Block::new(None, sender.clone(), BTreeSet::new())),
+            None,
         );
         // println!("Sender: {:?}", sender);
         // println!("Ghost calc: {:?}", ghost);
         // println!("Justification: {:?}", justification);
-        let m = BlockMsg::new(sender.clone(), justification, estimate);
+        let m = M::new(sender.clone(), justification, estimate);
         // println!("Message added: {:?}\n", m);
         let (_, sender_state) = Justification::from_msgs(
             LatestMsgsHonest::from_latest_msgs(
